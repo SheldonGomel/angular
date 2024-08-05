a0d263817bca8f7423fd60f22d146aa1e08b6204
Vitali Starastsenka <sheldongomel@gmail.com>
2024-08-05 13:54:06 +0300

    add withHashLocation to router provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { youtubeApiInterceptor } from './youtube/services/youtube-api.interceptor';
@@ -8,7 +7,7 @@ import { youtubeApiInterceptor } from './youtube/services/youtube-api.intercepto
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([youtubeApiInterceptor])),
   ],
 };
